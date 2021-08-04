a185acbb3d7e33cc0e18f95ed3c6000f70f57ed9
Wakko <80786738+wakkoTheWarner@users.noreply.github.com>
2021-08-03 23:05:49 -0400

    Clean up
    Cleaning up the Program like removing unnecessary code and using enhanced Switch statement.

--- passwordGenerator/src/Main.java
+++ passwordGenerator/src/Main.java
@@ -6,7 +6,6 @@
  * Version: 1.1
  */
 
-import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 public class Main {
 
@@ -34,57 +33,42 @@ public class Main {
         String specialLetters = "@#$";
         String password = "";
         int randomizer;
-        boolean passwordValidator = false;
+        boolean passwordValidator;
         int userNum = 0;
-        String userInput;
 
         // code
         while (userNum == 0) { // this while will verify if the number chosen by the user is long enough. If not, the program will ask the user to try again.
             System.out.print("How long should your prefered password be? (a good password should be from 6 - 18) ");
             userNum = scanner.nextInt();
-            if (userNum <6) {
+            if (userNum < 6) {
                 System.out.println(userNum + " is too low! Please try again!");
                 userNum = 0;
-                passwordValidator = true;
             } else { // if the number chosen by the user is good enough, the program will run correctly as it should.
                 passwordValidator = false;
-                while (passwordValidator == false) {
+                while (!passwordValidator) {
                     password = "";
                     for (int i = 1; i <= userNum; i++) {
                         randomizer =(int)(4 * Math.random());
-                        switch (randomizer) {
-                            case 0:
-                                randomizer = (int)(capitalLetters.length() * Math.random());
+                        switch (randomizer) { // used new enhanced switch statement to build the password given the specifications of the user
+                            case 0 -> {
+                                randomizer = (int) (capitalLetters.length() * Math.random());
                                 password += String.valueOf(capitalLetters.charAt(randomizer));
-                                break;
-                            case 1:
-                                randomizer = (int)(lowercaseLetters.length() * Math.random());
+                            }
+                            case 1 -> {
+                                randomizer = (int) (lowercaseLetters.length() * Math.random());
                                 password += String.valueOf(lowercaseLetters.charAt(randomizer));
-                                break;
-                            case 2:
-                                randomizer = (int)(numbers.length() * Math.random());
+                            }
+                            case 2 -> {
+                                randomizer = (int) (numbers.length() * Math.random());
                                 password += String.valueOf(numbers.charAt(randomizer));
-                                break;
-                            case 3:
-                                randomizer = (int)(specialLetters.length() * Math.random());
+                            }
+                            case 3 -> {
+                                randomizer = (int) (specialLetters.length() * Math.random());
                                 password += String.valueOf(specialLetters.charAt(randomizer));
-                                break;
+                            }
                         }
                     }
-                    if (checkingPasswords(password) == false) { // removed user request to see failed password for ease of use.
-                        /* System.out.println("\nOh uh! The password generated seems to fail the verification test!");
-                        System.out.print("Would you still want to generate it? ");
-                        userInput = scanner.next();
-                        if (userInput.equalsIgnoreCase("yes")) {
-                            passwordValidator = true;
-                        } else {
-                            passwordValidator = false;
-                        }
-                         */
-                        passwordValidator = false;
-                    } else {
-                        passwordValidator = true;
-                    }
+                    passwordValidator = checkingPasswords(password);
                 }
             }
         }
@@ -96,12 +80,31 @@ public class Main {
     public static boolean checkingPasswords(String password) {
         // variables
         passwordChecker passwordValidator = new passwordChecker(password);
-        boolean results = true;
+        boolean results;
 
         // code
         results = passwordValidator.findComplexity();
         return results;
     }
+
+    /*
+    public static boolean userVerification() {
+        // variables
+        String userInput;
+        boolean passwordValidator;
+
+        // code
+        System.out.println("\nOh uh! The password generated seems to fail the verification test!");
+        System.out.print("Would you still want to generate it? ");
+        userInput = scanner.next();
+        if (userInput.equalsIgnoreCase("yes")) {
+            passwordValidator = true;
+        } else {
+            passwordValidator = false;
+        }
+        return passwordValidator;
+    }
+     */
 
     public static void sayingGoodbye() {
         System.out.println("\n*********************");
